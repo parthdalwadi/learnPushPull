5af1eb8fc60881de1e7bcae948cf38aba60676a6
Parth Dalwadi <pbdalwadi@gmail.com>
2020-09-01 22:54:03 -0400

    Initial Commit

---
+++ learnGIT/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  learnGIT
+//
+//  Created by Parth Dalwadi on 2020-09-01.
+//  Copyright © 2020 Parth Dalwadi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
